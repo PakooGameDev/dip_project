e76be647ed6648ace13a10306acc8452f5166c51
PakooGD <popkov-lelya89@mail.ru>
2023-08-23 22:40:55 +0300

    feat: removed js

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,3 @@
 import './vendor';
 
 import styles from './app.module.css';
-import { drawSidebar } from './sidebar/sidebar';
-
-const body = document.body;
-body.className = styles.body;
-
-drawSidebar();
